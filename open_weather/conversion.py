db07cc877529310bef5bb5c009902fcb39e1d847
Ayush Gupta <72538480+GuptaAyush19@users.noreply.github.com>
2021-09-15 14:04:17 +0530

    Update conversion.py

--- open_weather/conversion.py
+++ open_weather/conversion.py
@@ -10,4 +10,4 @@ def to_celcius(kelvin:float, round_digit=2) -> float:
     """       
     return round(kelvin-273.15, round_digit)
 
-#TODO: Add a function to convert Fahrenheit to Celcius 
+#TODO: Add a function to convert Fahrenheit to Celcius and vice versa
