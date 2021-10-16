3dace45818c77a0f36f3c92c4e4fb83a13bbad4d
Sayanjit Ukil <sayanjitukil2005@gmail.com>
2021-10-16 13:08:42 +0530

    Updated GUI

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import datetime 
 import tkinter as tk
+import ctypes
 
 from time import time as _time
 
@@ -24,83 +25,87 @@ class Persistence:
 
 class Weather(tk.Tk):
 
-    @property
-    def font(self):
-        return ('calibri',20,'bold')
     
     def __init__(self):
         super().__init__()
 
-        # configure the root window
+        # configuring the window
         self.geometry('500x700')
-        self.title("WEATHER REPORT")
-        self.config(bg='black')
-        
-        # add location
-        self.location=Label(self,text="NA-/",bg="black",fg="white",font=self.font)
-        self.location.place(x=0,y=0)
+        self.config(bg="#F0F3F4")
+        self.resizable(0,0)
 
-        # add heading
-        self.heading=Label(self,text="Weather Report",bg="black",fg="white",font=self.font)
-        self.heading.place(x=180,y=0)
-        
-        # date,time,month label
-        dt=datetime.datetime.now()
-        currentdate=Label(self,text=dt.strftime('%A--'),bg='black',fg='orange',font=("bold ",15))
-        currentdate.place(x=5,y=230)
-        month=Label(self,text=dt.strftime('%m%B'),bg='black',fg='orange',font=("bold ",15))
-        month.place(x=95,y=230)
-        hour=Label(self,text=dt.strftime('%I : %M %p'),bg='white',font=("bold",15))
-        hour.place(x=10,y=160)
-        
-        # add name
-        self.name=Label(self,text="Enter City or Country name ",fg='orange',bg='black',font=self.font)
-        self.name.place(x=100,y=35)
+        # setting the window icon(top)
+        windowlogo=PhotoImage(file=r"C:\Users\user\Desktop\weather-update\images\icon.png")
+        self.iconphoto(True,windowlogo)
 
-        self.text_box=Text(self,width=25,height=2)
-        self.text_box.place(x=150,y=75)       
-        
+        # text field for user to enter
+        self.city=Entry(self,width=25,font=("Cambria,bold",11))
+        self.city.place(x=150,y=0)
 
-       # city name
-        city_name=Label(self,text="....",width=0,bg='black',fg='orange',font=("bold,15"))
-        city_name.place(x=0,y=100)
+        # setting the taskbar icon
+        logo = r"C:\Users\user\Desktop\weather-update\images\icon.png" # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(logo)
 
-        # temperature
-        temperature=Label(self,text="...",width=0,bg="black",font=("Helvetica",15), fg="orange")
-        temperature.place(x=18,y=300)
+        # setting date,time and year
+        dt=datetime.datetime.now() 
+        currentdate=Label(self,text=dt.strftime('%A,'), bg='#F0F3F4',fg='black',font=("Cambria,bold",12))
+        currentdate.place(x=160,y=160)
+        month=Label(self,text=dt.strftime('%m %B'),bg='#F0F3F4',fg='black' ,font=("Cambria,bold",12))
+        month.place(x=230,y=160)
 
-        # maximum temperature
-        maxtemp=Label(self,text="MAXIMUM TEMPERATURE: ",width=0,bg="black",fg="orange",font=("bold,15"))
-        maxtemp.place(x=0,y=400)
+        # space to receive city details
+        self.location=Label(self, text="...",width=0,bg='#F0F3F4',fg='black', font=("bold", 20))
+        self.location.place(x=225,y=100)
 
-        # maximum temperature
-        # temperature=Label(self,text="...",width=0,bg="black",font=("Helvetica",15),fg="orange")
-        # temperature.place(x=0,y=450)
+        # openweather icons
+        icon=Label(self,text="... ",width=0,bg='#F0F3F4',font=("bold",50))
+        icon.place(x=140,y=260)
 
-        # minimum temperature
-        mintemp=Label(self,text="MINIMUM TEMPERATURE: ",width=0,bg="black",fg="orange",font=("bold,15"))
-        mintemp.place(x=0,y=500)
+        # space to receive temperature
+        self.temperature = Label(self, text="...", width=0, bg='#F0F3F4',font=("Cambria,bold", 50), fg='black')
+        self.temperature.place(x=270, y=260)
 
-        # #gets minimum temperature
-        # temperature=Label(self,text="...",width=0,bg='black',font=("Helvetica",15),fg='orange')
-        # temperature.place(x=0,y=550)
+        # humidity
+        humidity_display=Label(self,text="Humidity ",width=0,bg='#F0F3F4',fg='black',font=("Cambria,bold",12))
+        humidity_display.place(x=100,y=400)
 
-        # description
-        self.des=Label(self,text="DESCRIPTION: ",bg='black',fg='orange',font=("bold,15"))
-        self.des.place(x=0,y=600)
+        # space to display humidity
+        self.humidity=Label(self,text="...",width=0,bg='#F0F3F4',font=("bold,Cambria",15),fg="black")
+        self.humidity.place(x=200,y=400)
 
-        self.description=Label(self,text="...", width=24,bg='white',font=("bold,17"),fg="black")
-        self.description.place(x=230,y=600)
+        # UV index
+        uvindex_display=Label(self,text="UV Index ",width=0,bg='#F0F3F4',fg='black',font=("Cambria,bold",12))
+        uvindex_display.place(x=100,y=450)
+
+        #space to display UV index
+        uvindex=Label(self,text="...",width=0,bg='#F0F3F4',font=("bold,Cambria",15),fg="black")
+        uvindex.place(x=200,y=450)
+
+        # probability of rain
+        rainprobability_display=Label(self,text="Probaility of rain ",width=0,bg='#F0F3F4',fg='black',font=("Cambria,bold",12))
+        rainprobability_display.place(x=100,y=500)
+
+        # space to display rain probability
+        rainprobability=Label(self,text="...",width=0,bg='#F0F3F4',font=("bold,Cambria",15),fg="black")
+        rainprobability.place(x=250,y=500)
+
+         # pressure
+        pressure_display=Label(self,text="Pressure ",width=0,bg='#F0F3F4',fg='black',font=("Cambria,bold",12))
+        pressure_display.place(x=100,y=550)
+
+        # space to display pressure
+        self.pressure=Label(self,text="...",width=0,bg='#F0F3F4',font=("Cambria,bold",15), fg='black')
+        self.pressure.place(x=200,y=550)
 
         self.main_method(city_name=Persistence.get_data())
 
         # search button
         self.button=Button(self, text="Search",bg="black",fg='white',font=("bold,12"))
         self.button["command"] = self.main_method
-        self.button.place(x=355,y=75)
+        self.button.place(x=300,y=0)
 
     def main_method(self, city_name=None):
-        if city_name == None: city_name:str = self.text_box.get(1.0, END)
+        if city_name == None: city_name:str = self.city.get()
             # save the current input city name to data directory
 
         city_obj = CurrentCity(city_name, test=False)
@@ -109,20 +114,20 @@ class Weather(tk.Tk):
 
         self.location['text'] = city_obj.name
 
-        self.des['text'] = city_obj.weather['main']
-        self.des['font'] = ('calibri',20,'bold')
+        #self.des['text'] = city_obj.weather['main']
+        #self.des['font'] = ('calibri',20,'bold')
 
-        self.description['text'] = city_obj.weather['description']
-        self.description['font'] = ('calibri',20,'bold')
+        #self.description['text'] = city_obj.weather['description']
+        #self.description['font'] = ('calibri',20,'bold')
 
-        # self.temperature['text'] = city_obj.temperature
-        # self.temperature['font'] = ('calibri',15,'bold')
+        self.temperature['text'] = city_obj.temperature
+        self.temperature['font'] = ('calibri',15,'bold')
 
-        # self.humidity['text'] = city_obj.humidity
-        # self.humidity['font'] = ('calibri',15,'bold')
+        self.humidity['text'] = city_obj.humidity
+        self.humidity['font'] = ('calibri',15,'bold')
 
-        # self.pressure['text'] = city_obj.pressure
-        # self.pressure['font'] = ('calibri',15,'bold')
+        self.pressure['text'] = city_obj.pressure
+        self.pressure['font'] = ('calibri',15,'bold')
 
         Persistence.set_data(city_name)
 
